3759ed94741566d5c5f72c89b480396aa9295005
Chloe-Ms <mllechloe.mathias@gmail.com>
2023-05-26 10:26:06 +0200

    Hold touch added

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,44 +1,72 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    [SerializeField] private InputActionReference _touchPress;
-    [SerializeField] private InputActionReference _touchPosition;
-    [SerializeField] GameObject _gameObject;
-    public event Action OnStartTouchEvent;
-    public event Action OnEndTouchEvent;
+    [SerializeField] InputActionReference _touchPut;
+    [SerializeField] InputActionReference _touchPosition;
+    [SerializeField] InputActionReference _touchPress;
+    [SerializeField] GameObject _gameObject; //Pour le test, à enlever plus tard
+    
+    Coroutine _holdRoutine;
+    bool _hasHoldInputStarted = false;
 
-    void Awake()
-    {
-        _touchPress.action.performed += OnTouchStarted;
+    public event Action OnTapEvent;
+    public event Action OnHoldStartedEvent;
+    public event Action OnHoldEndedEvent;
+
+    private void OnEnable()
+    {  
+        _touchPut.action.performed += OnTouchTap;
+        _touchPress.action.performed += OnHoldStarted;
+        _touchPress.action.canceled += OnHoldEnded;
     }
 
-    void OnDestroy()
+    private void OnDisable()
     {
-        _touchPress.action.performed -= OnTouchStarted;
-    }
-    private void OnTouchEnded(InputAction.CallbackContext obj)
-    {
-        Debug.Log("Touch ended " + _touchPosition);
-        OnEndTouchEvent?.Invoke();
+        _touchPut.action.performed -= OnTouchTap;
     }
 
-    private void OnTouchStarted(InputAction.CallbackContext obj)
+    private void OnTouchTap(InputAction.CallbackContext obj)
     {
+        Vector2 value = _touchPosition.action.ReadValue<Vector2>();
+        Vector3 position = Camera.main.ScreenToWorldPoint(value);
+        //Debug.LogWarning("Tap position " + position + " value " + value);
+        position.z = 0;
+        _gameObject.transform.position = position;
+
+        OnTapEvent?.Invoke();
+    }
+    private void OnHoldStarted(InputAction.CallbackContext obj)
+    {
+        Vector2 value = _touchPosition.action.ReadValue<Vector2>();
+        Vector3 position = Camera.main.ScreenToWorldPoint(value);
+        //Debug.LogWarning("Press start position " + position + " value " + value);
+        _holdRoutine = StartCoroutine(StartDrag());
+        OnHoldStartedEvent?.Invoke();
     }
 
-    private void Update()
+    private void OnHoldEnded(InputAction.CallbackContext obj)
     {
-        if (_touchPress.action.WasPerformedThisFrame())
+        if (_hasHoldInputStarted && _holdRoutine != null)
+        {
+            StopCoroutine(_holdRoutine);
+            _holdRoutine = null;
+            OnHoldEndedEvent?.Invoke();
+        }
+    }
+
+    IEnumerator StartDrag()
+    {
+        while (true)
         {
             Vector2 value = _touchPosition.action.ReadValue<Vector2>();
-
             Vector3 position = Camera.main.ScreenToWorldPoint(value);
-            Debug.LogWarning("position " + position + " value " + value);
             position.z = 0;
             _gameObject.transform.position = position;
+            yield return new WaitForFixedUpdate();
         }
     }
 }
