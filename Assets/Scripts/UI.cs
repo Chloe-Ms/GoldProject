8bf2e768bfe06cfae024bcfbc474edc1a75f858f
Rakan <76219145+RakanAD@users.noreply.github.com>
2023-05-31 10:34:45 +0200

    UI implementation

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] GameObject _mainMenu;
+    [SerializeField] GameObject _optionMenu;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Debug.Log("Play");
+    }
+
+    public void Option()
+    {
+        _mainMenu.SetActive(false);
+        _optionMenu.SetActive(true);
+        Debug.Log("Option");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void OptionMenu()
+    {
+        _mainMenu.SetActive(true);
+        _optionMenu.SetActive(false);
+        Debug.Log("OptionMenu");
+    }
+}
