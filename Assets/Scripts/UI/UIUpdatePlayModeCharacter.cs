0a48de9302de39488c7d0c86c893bec4c81b8201
Chloe-Ms <mllechloe.mathias@gmail.com>
2023-06-13 01:38:56 +0200

    Test input + fix ui pv lost

--- Assets/Scripts/UI/UIUpdatePlayModeCharacter.cs
+++ Assets/Scripts/UI/UIUpdatePlayModeCharacter.cs
@@ -11,7 +11,15 @@ public class UIUpdatePlayModeCharacter : MonoBehaviour
     [SerializeField] TextMeshProUGUI _feedbackPV;
     [SerializeField] float _offsetYDamage = 0.5f;
     [SerializeField, Min(0.1f)] float _durationMovementDamage = 1f;
+    float _startPosY;
 
+    private void Start()
+    {
+        if (_feedbackPV != null)
+        {
+            _startPosY = _feedbackPV.transform.position.y;
+        }
+    }
     public void Init(Hero hero)
     {
         _heroHead.sprite = hero.HeadSprite;
@@ -29,16 +37,18 @@ public class UIUpdatePlayModeCharacter : MonoBehaviour
     {
         _feedbackPV.gameObject.SetActive(true);
         _feedbackPV.text = damageTaken.ToString();
-        float startPosY = _feedbackPV.transform.position.y;
-        _feedbackPV.transform.DOMoveY(startPosY+ _offsetYDamage,_durationMovementDamage)
+        _feedbackPV.transform.DOMoveY(_startPosY+ _offsetYDamage,_durationMovementDamage)
             .OnComplete(() =>
             {
                 _feedbackPV.gameObject.SetActive(false);
                 _feedbackPV.transform.position = new Vector3(
                     _feedbackPV.transform.position.x, 
-                    startPosY, 
+                    _startPosY, 
                     _feedbackPV.transform.position.z);
             });
-        _feedbackPV.DOFade(0, _durationMovementDamage).SetEase(Ease.InExpo);
+        _feedbackPV.DOFade(0, _durationMovementDamage).SetEase(Ease.InExpo).OnComplete(() =>
+        {
+            _feedbackPV.alpha = 1;
+        });
     }
 }
